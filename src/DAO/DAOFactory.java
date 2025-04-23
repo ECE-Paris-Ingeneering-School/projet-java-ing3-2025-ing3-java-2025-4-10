f2d2ab6a73ff55689d613205c233d6abaa4f6263
kimih <kinmiho168@hotmail.com>
2025-04-23 13:58:27 +0200

    Ajout DAOFactory

--- src/DAO/DAOFactory.java
+++ src/DAO/DAOFactory.java
@@ -1,5 +1,9 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DAOFactory {
     private static String url;
     private String username;
@@ -12,6 +16,74 @@ public class DAOFactory {
         this.password = password;
     }
 
+    /**
+     * Méthode qui retourne 1 objet de DaoFactory
+     * @param : url, username et password de la base de données
+     * @return : objet de la classe DaoFactoru
+     */
+    public static DAOFactory getInstance(String database, String username, String password) {
+        try {
+            // chargement driver "com.mysql.cj.jdbc.Driver"
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Erreur de connexion à la base de données");
+        }
+
+        url = "jdbc:mysql://localhost:3306/" + database;
+
+        // Instancier une instance l'objet de DaoFactory
+        DAOFactory instance = new DAOFactory(url, username,password );
+
+        // Retourner cette instance
+        return instance;
+    }
+
+    /**
+     * Méthode qui retourne le driver de base de données approprié
+     * @return : le driver approprié
+     * @throws SQLException
+     */
+    public Connection getConnection() throws SQLException {
+        // Retourner la connection du driver de la base de données
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    /**
+     * Récupération du Dao pour les RDV disponibles
+     * @return : objet de la classe ProduitDAOImpl
+     */
+    //A coder
+
+
+    /**
+     * Récupération du Dao pour les Prises de RDV
+     * @return : objet de la classe ProduitDAOImpl
+     */
+    //a coder
+
+    /**
+     * Récupération du Dao pour les Patients
+     * @return : objet de la classe ProduitDAOImpl
+     */
+    //a coder
+
+
+
+
+    //Fermer la connexion a la base de données
+    public void disconnect() {
+        Connection connexion = null;
+
+        try {
+            // création d'un ordre SQL (statement)
+            connexion = this.getConnection();
+            connexion.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erreur de déconnexion à la base de données");
+        }
+    }
 }
 
 
