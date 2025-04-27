7cb7f196c2564fa473f2f88268a16ba30ceab915
henridav <149801170+henridav@users.noreply.github.com>
2025-04-27 09:26:10 +0200

    commentaires Note.java

--- src/modele/Note.java
+++ src/modele/Note.java
@@ -1,18 +1,30 @@
-package modele;
+package modele; // package modele
 
-public class Note {
-    private int idNote;
-    private String commentaire;
-    private int idRDV;
+public class Note { // classe note
 
+    private int idNote; // id unique de la note
+    private String commentaire; // texte du commentaire
+    private int idRDV; // id du rendez-vous lié
+
+    // constructeur
     public Note(int idNote, String commentaire, int idRDV) {
         this.idNote = idNote;
         this.commentaire = commentaire;
         this.idRDV = idRDV;
     }
 
-    //getters
-    public int getIdNote() { return idNote;}
-    public String getCommentaire() { return commentaire;}
-    public int getIdRDV() { return idRDV;}
+    // === getters ===
+
+    public int getIdNote() { // récupérer l'id de la note
+        return idNote;
+    }
+
+    public String getCommentaire() { // récupérer le commentaire
+        return commentaire;
+    }
+
+    public int getIdRDV() { // récupérer l'id du rendez-vous associé
+        return idRDV;
+    }
 }
+
