4b76817fb0d0d490c1683da98504c5d93d863ad2
henridav <149801170+henridav@users.noreply.github.com>
2025-04-27 09:43:53 +0200

    Update GestionRendezVousVue.java

--- src/vue/GestionRendezVousVue.java
+++ src/vue/GestionRendezVousVue.java
@@ -1,4 +1,4 @@
-package vue; // package vue
+package vue;
 
 import controleur.RendezVousControleur; // importation du controleur pour les rendez-vous
 import modele.RendezVous; // importation du modele rendezvous
