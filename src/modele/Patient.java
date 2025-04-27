51e49d8091830fcf3463e73704a1aab659587279
henridav <149801170+henridav@users.noreply.github.com>
2025-04-27 09:27:01 +0200

    commentaires Patient.java

--- src/modele/Patient.java
+++ src/modele/Patient.java
@@ -1,47 +1,47 @@
-package modele;
+package modele; // package modele
 
-public class Patient {
+public class Patient { // classe patient
 
-    private int idUser;        // Identifiant utilisateur (lié à la table User)
-    private String nom;
-    private String prenom;
+    private int idUser; // identifiant utilisateur (lié à la table User)
+    private String nom; // nom du patient
+    private String prenom; // prenom du patient
 
+    // constructeur
     public Patient(int idUser, String nom, String prenom) {
         this.idUser = idUser;
         this.nom = nom;
         this.prenom = prenom;
     }
-    
 
-    // === GETTERS ===
+    // === getters ===
 
-    public int getIdUser() {
+    public int getIdUser() { // récupérer l'id user
         return idUser;
     }
 
-    public String getNom() {
+    public String getNom() { // récupérer le nom
         return nom;
     }
 
-    public String getPrenom() {
+    public String getPrenom() { // récupérer le prénom
         return prenom;
     }
 
-    // === SETTERS ===
+    // === setters ===
 
-    public void setNom(String nom) {
+    public void setNom(String nom) { // changer le nom
         this.nom = nom;
     }
 
-    public void setPrenom(String prenom) {
+    public void setPrenom(String prenom) { // changer le prénom
         this.prenom = prenom;
     }
 
-    public void setIdUser(int idUser) {
+    public void setIdUser(int idUser) { // changer l'id user
         this.idUser = idUser;
     }
 
-    // === Pour affichage ou debug éventuel ===
+    // affichage lisible pour debug ou affichage
     @Override
     public String toString() {
         return prenom + " " + nom + " (ID: " + idUser + ")";
