eb2c8d9cd5aa4c28172d63ac3d37354030f63df0
henridav <149801170+henridav@users.noreply.github.com>
2025-04-27 09:38:51 +0200

    commentaires SpecialisteControleur.java

--- src/controleur/SpecialisteControleur.java
+++ src/controleur/SpecialisteControleur.java
@@ -1,22 +1,25 @@
 package controleur;
 
+// importation des classes nécessaires
 import dao.SpecialisteDAO;
 import modele.Specialiste;
-
 import java.util.List;
 
-public class SpecialisteControleur {
+public class SpecialisteControleur { // classe controleur pour gérer les spécialistes
 
-    private final SpecialisteDAO dao = new SpecialisteDAO();
+    private final SpecialisteDAO dao = new SpecialisteDAO(); // instance de SpecialisteDAO
 
+    // récupère tous les spécialistes
     public List<Specialiste> getTousLesSpecialistes() {
         return dao.recupererTousLesSpecialistes();
     }
 
+    // ajoute un spécialiste
     public boolean ajouterSpecialiste(Specialiste s) {
         return dao.ajouterSpecialiste(s);
     }
 
+    // supprime un spécialiste par son id
     public boolean supprimerSpecialisteParId(int id) {
         return dao.supprimerSpecialisteParId(id);
     }
